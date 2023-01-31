8937afdf3f7e4cade1ae4964f3fc54afdb96d249
Ania Dermin <66634563+m3right@users.noreply.github.com>
2023-01-31 18:47:47 +0000

    1. Została utworzona lista do której zapisano dwa nowe obiekty klasy osoba, o3 i o4, które również zostały utworzone 2. zostal utworzony plik2.ser do którego została zapisana o3 a następnie wyświetlona na ekran

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.io.ObjectOutputStream;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.io.IOException;
 
 
@@ -12,28 +13,47 @@ public class Main {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		Osoba o1 = new Osoba("Adam", "Mickiewicz", 1798);
 		Osoba o2 = new Osoba("Juliusz", "Slowacki", 1809);
-		
+    Osoba o3 = new Osoba("Anna", "Dermin", 1999);
+    Osoba o4 = new Osoba("Rafał", "Pot", 1994);
+
+    ArrayList<Osoba> osoby = new ArrayList<Osoba>();
+    osoby.add(o3);
+    osoby.add(o4);
 	
 		FileOutputStream fos = new FileOutputStream("plik.ser");
+    FileOutputStream wos = new FileOutputStream ("plik2.ser");
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
+    BufferedOutputStream los = new BufferedOutputStream(wos);
 		ObjectOutputStream oos = new ObjectOutputStream(bos);
+    ObjectOutputStream os = new ObjectOutputStream(los);
 		
 		oos.writeObject(o1);
 		oos.writeObject(o2);
+    os.writeObject(o3);
 		
 		oos.close();
-		
+    os.close();
+ 
+    
 		FileInputStream fis = new FileInputStream("plik.ser");
+    FileInputStream wis = new FileInputStream("plik2.ser");
 		BufferedInputStream bis = new BufferedInputStream(fis);
+    BufferedInputStream lis = new BufferedInputStream(wis);
 		ObjectInputStream ois = new ObjectInputStream(bis);
-				
-		while(bis.available() > 0) {
+    ObjectInputStream oi = new ObjectInputStream(lis);
+    
+    while(bis.available() > 0) {
 			Osoba o = (Osoba) ois.readObject();
 			System.out.println(o);
 		}
-		
-		ois.close();
 
+    while(lis.available()>0){
+      Osoba o = (Osoba) oi.readObject();
+      System.out.println(o);
+    }
+			
+		ois.close();
+    oi.close();
 	}
 }
 
